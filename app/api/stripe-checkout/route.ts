cae0bb5365ef8f08196afcf7cb523c55ca6d4f80
Rana-Hassan7272 <ssc.shahbaz.2004@gmail.com>
2025-09-22 17:30:26 +0500

    dkj

--- app/api/stripe-checkout/route.ts
+++ app/api/stripe-checkout/route.ts
@@ -1,9 +1,7 @@
 import { type NextRequest, NextResponse } from "next/server"
 import Stripe from "stripe"
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2025-08-27.basil",
-})
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!)
 
 export async function GET(request: NextRequest) {
   const { searchParams } = new URL(request.url)
